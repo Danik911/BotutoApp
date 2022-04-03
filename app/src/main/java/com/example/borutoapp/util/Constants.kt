40f7b1e8f1456628e2e59a23641de9d2d23d11a1
Danik <porvipop@mail.ru>
2022-04-03 15:47:03 +0300

    Add DI

--- app/src/main/java/com/example/borutoapp/util/Constants.kt
+++ app/src/main/java/com/example/borutoapp/util/Constants.kt
@@ -5,4 +5,6 @@ object Constants {
     const val HERO_ID_ARGUMENT_KEY = "heroId"
     const val HERO_DATABASE_TABLE = "hero_database_table"
     const val HERO_REMOTE_KEY_TABLE = "hero_remote_key_table"
+
+    const val BORUTO_DATABASE = "boruto_database"
 }
