9f4e535973846df9d1a217aa2b26a9bc4ba275f2
Danik <porvipop@mail.ru>
2022-04-10 08:04:34 +0300

    Create Network Module

--- app/src/main/java/com/example/borutoapp/util/Constants.kt
+++ app/src/main/java/com/example/borutoapp/util/Constants.kt
@@ -2,6 +2,8 @@ package com.example.borutoapp.util
 
 object Constants {
 
+    const val BASE_URL = "https://10.0.2.2:8080"
+
     const val HERO_ID_ARGUMENT_KEY = "heroId"
     const val HERO_DATABASE_TABLE = "hero_database_table"
     const val HERO_REMOTE_KEY_TABLE = "hero_remote_key_table"
