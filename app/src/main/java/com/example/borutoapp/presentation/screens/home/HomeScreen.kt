facaae451f0f12faf72a15ec28d34b2fecfaa1ab
Daniil <porvipop@mail.ru>
2022-04-16 14:09:04 +0300

    Add calculate function into RatingWidget

--- app/src/main/java/com/example/borutoapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/borutoapp/presentation/screens/home/HomeScreen.kt
@@ -1,14 +1,18 @@
 package com.example.borutoapp.presentation.screens.home
 
 import android.util.Log
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.example.borutoapp.presentation.components.RatingWidget
 import com.example.borutoapp.presentation.screens.splash.SplashViewModel
 
 @Composable
@@ -23,6 +27,8 @@ fun HomeScreen(
             HomeAppTopBar(onSearchClicked = {})
         }
     ) {
+
+        RatingWidget(modifier = Modifier.padding(8.dp), rating = 2.3)
 
     }
 }
--- app/src/main/java/com/example/borutoapp/ui/theme/Dimens.kt
+++ app/src/main/java/com/example/borutoapp/ui/theme/Dimens.kt
@@ -2,6 +2,7 @@ package com.example.borutoapp.ui.theme
 
 import androidx.compose.ui.unit.dp
 
+val PADDING_SMALLEST = 6.dp
 val PADDING_SMALL = 10.dp
 val PADDING_MEDIUM = 16.dp
 val PADDING_LARGE = 20.dp
