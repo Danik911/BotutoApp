f090ad5bc057c2473f86d12d92fa9b8389e39823
Daniil <porvipop@mail.ru>
2022-05-05 10:53:59 +0300

    Add heroku API

--- app/src/main/java/com/example/borutoapp/util/Constants.kt
+++ app/src/main/java/com/example/borutoapp/util/Constants.kt
@@ -2,7 +2,8 @@ package com.example.borutoapp.util
 
 object Constants {
 
-    const val BASE_URL = "http://10.0.2.2:8080"
+    //const val BASE_URL = "http://10.0.2.2:8080"
+    const val BASE_URL = "http://ktor-boruto-app.herokuapp.com/"
 
     const val HERO_ID_ARGUMENT_KEY = "heroId"
     const val HERO_DATABASE_TABLE = "hero_database_table"
